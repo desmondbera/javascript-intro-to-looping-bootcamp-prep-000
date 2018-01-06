babc86e98706786e4d6c95bdd714509515c982d1
desmond beramendi <desmond7187@gmail.com>
2018-01-06 04:15:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,9 @@
 const arr = []
 
-function forLoop() {
+function forLoop(array) {
   
+  for(var i = 0; i < 25; i++) {
+    return `I am ${i} strange loop${i === 0 ? '' : 's'}`
+  }
 }
 
-for(var i = 0; i < 25; i++) {
-  arr.push[i];
-  return arr;
-}
