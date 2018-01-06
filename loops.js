02a0d7c871158a53fb52ddb9d978217da913f55a
desmond beramendi <desmond7187@gmail.com>
2018-01-06 04:19:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
-const arr = []
-
 function forLoop(array) {
-  
-  for(var i = 0; i < 25; i++) {
-    return `I am ${i} strange loop${i === 0 ? '' : 's'}`
+  for(let i = 0; i < 25; i++) {
+    
   }
 }
+
+
+
 
